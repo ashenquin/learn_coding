47cdb09fb34fd46eef2f2e8e7b47fa589dfd072f
ashenquin <2593810323@qq.com>
2019-03-13 23:21:18 +0800

    learn js_base and bootstrap and interface_base

--- algorithm/qq/index.js
+++ algorithm/qq/index.js
@@ -2,25 +2,25 @@
 let enc_qq =[6,3,1,7,5,8,9,2,4],
     qq = []; //真正的QQ
     head=0;//队首指针，要移除的元素位置
-    tail=9;//队尾指针，要加入的元素位置
+    tail=8;//队尾指针，要加入的元素位置
 // 第一个数移除
-while (head<tail) 
-{   qq.push(enc_qq[head]);
-    head++;
-    enc_qq[tail] = enc_qq[head];
-    tail++;
-    head++;
-    console.log(qq);
-}
-console.log(typeof qq);
-// while(head<tail){
-//     qq.push(enc_qq[head]);
-//     enc_qq.shift();
-//     enc_qq[tail]=enc_qq[head];
-//     enc_qq.shift();
-//     tail--;
+// while (head<tail) 
+// {   qq.push(enc_qq[head]);
+//     head++;
+//     enc_qq[tail] = enc_qq[head];
+//     tail++;
+//     head++;
 //     console.log(qq);
 // }
+// console.log(typeof qq);
+while(head<=tail){
+    qq.push(enc_qq[head]);
+    enc_qq.shift();
+    enc_qq[tail]=enc_qq[head];
+    enc_qq.shift();
+    tail--;
+    console.log(qq);
+}
 // 移除数组的第一个数字
 // enc_qq.shift();
 // 在数组前插入元素
